6b2760981ebe606a674bc864a064c047cd1b67a9
Adel <yassinadel@hotmail.com>
2022-05-04 11:21:44 +0300

    project initialized

---
+++ app/src/main/java/com/csc498/mysql/MainActivity.java
@@ -0,0 +1,22 @@
+package com.csc498.mysql;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    try{
+
+    }catch (Exception e){
+
+    }
+
+
+    }
+}
