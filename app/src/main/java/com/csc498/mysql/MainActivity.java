4b7788ea411800ab736835c6980979f1a764c9af
Adel <yassinadel@hotmail.com>
2022-05-04 13:03:19 +0300

    application done

--- app/src/main/java/com/csc498/mysql/MainActivity.java
+++ app/src/main/java/com/csc498/mysql/MainActivity.java
@@ -2,9 +2,12 @@ package com.csc498.mysql;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -47,10 +50,23 @@ public class MainActivity extends AppCompatActivity {
             cursor.moveToNext();
         }
         listView.setAdapter(adapter);
+
+
+
+
     }catch (Exception e){
         e.printStackTrace();
     }
+        Intent intent = new Intent(getApplicationContext(),Webview.class);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                startActivity(intent);
+
+            }
+        });
 
     }
 }
--- app/src/main/java/com/csc498/mysql/Webview.java
+++ app/src/main/java/com/csc498/mysql/Webview.java
@@ -8,15 +8,19 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class Webview extends AppCompatActivity {
-    WebView view = (WebView) findViewById(R.id.webview);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_webview);
+        WebView view = (WebView) findViewById(R.id.webview);
+
+        view.getSettings().setJavaScriptEnabled(true);
+        view.setWebViewClient( new WebViewClient());
+        view.loadUrl("http://www.google.com");
+
+
     }
 
 
-//        view.getSettings().setJavaScriptEnabled(true);
-//        view.setWebViewClient( new WebViewClient());
-//        view.loadUrl("http://www.google.com");
 }
