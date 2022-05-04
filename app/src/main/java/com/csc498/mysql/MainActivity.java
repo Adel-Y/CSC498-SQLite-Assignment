c68edbfbd7d0a5c19eca9550b0f7b1617bd63cd5
Adel <yassinadel@hotmail.com>
2022-05-04 11:40:47 +0300

    cursor added

--- app/src/main/java/com/csc498/mysql/MainActivity.java
+++ app/src/main/java/com/csc498/mysql/MainActivity.java
@@ -2,6 +2,7 @@ package com.csc498.mysql;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
@@ -16,10 +17,15 @@ public class MainActivity extends AppCompatActivity {
 
         SQLiteDatabase sql = this.openOrCreateDatabase("finaldb",MODE_PRIVATE,null);
         sql.execSQL("CREATE Table IF NOT EXISTS exams(exam_name VARCHAR, exam_date VARCHAR)");
-        sql.execSQL("INSERT INTO exams(exam_name,exam_date) VALUES('mobile computing','May 13')");
-        sql.execSQL("INSERT INTO exams(exam_name,exam_date) VALUES('microeconomics','May 7')");
-    }catch (Exception e){
+//        sql.execSQL("INSERT INTO exams(exam_name,exam_date) VALUES('mobile computing','May 13')");
+//        sql.execSQL("INSERT INTO exams(exam_name,exam_date) VALUES('microeconomics','May 7')");
 
+        Cursor cursor = sql.rawQuery("SELECT * FROM exams",null);
+        int name_index= cursor.getColumnIndex("exam_name");
+        int date_index= cursor.getColumnIndex("exam_date");
+
+    }catch (Exception e){
+        e.printStackTrace();
     }
 
 
